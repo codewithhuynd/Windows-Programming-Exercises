10464fb10f9e5cf7489a400ddb5e80a10e9bb020
codewithhuynd <nguyenduchuy31052006@gmail.com>
2025-09-17 09:16:24 +0700

    create_bai1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Baitap1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao ten ban:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Xin chao " + name);
+        }
+    }
+}
