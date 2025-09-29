4d4b6565a5e86d1faef8165c86f17ce3ef1f013b
codewithhuynd <nguyenduchuy31052006@gmail.com>
2025-09-29 22:35:02 +0700

    Create_baitap1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao ten ban:");
+            string name = Console.ReadLine();
+            Console.Write("Xin chao " + name);
+        }
+    }
+}
